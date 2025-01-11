a17742ac133d460d9cc77d2491ea0ac466ac7882
Amir Hossein Samiee <celmir4444@gmail.com>
2025-01-11 23:23:52 +0330

    caculate_follows debugged, print_parse_table added

--- cfg.py
+++ cfg.py
@@ -1,5 +1,4 @@
-from rich import print
-from sympy import symbols
+from tabulate import tabulate
 from assets import *
 CFG_FILE_PATH = "cfg.txt"
 CFG_RULES_STR = ""
@@ -108,15 +107,14 @@ class CFG:
                         for x in self._first_values[f]:
                             if x == "":
                                 no_epsilon = False
-                                if f != rule.rest[-1]:
-                                    continue
-                            # if x not in self._first_values[rule.origin]:
-                            #     self._first_values[rule.origin].add(x)
-                            #     keep_calculating = True
+                                continue
                             keep_calculating = keep_calculating or add_if_not_already_added(
                                 x, self._first_values[rule.origin])
                         if no_epsilon:
                             break
+                else:
+                    add_if_not_already_added(
+                        "", self._first_values[rule.origin])
 
     def _calculate_follows(self):
         self._follow_values[Symbol("start")].add("$")
@@ -124,65 +122,50 @@ class CFG:
         while keep_calculating:
             keep_calculating = False
             for rule in self.rules:
-                has_epsilon = ["" in self.first(x) if isinstance(
-                    x, Symbol) else False for x in rule.rest]
                 for i in range(len(rule.rest)):
                     f = rule.rest[i]
                     if isinstance(f, str):
                         continue
-                    do = True
-                    while i + 1 < len(rule.rest) and (do or has_epsilon[i + 1]):
-                        do = False
-                        i += 1
-                        g = rule.rest[i]
-                        if isinstance(g, str):
-                            keep_calculating = keep_calculating or add_if_not_already_added(
-                                g, self._follow_values[f])
-                        else:
-                            for x in self.first(g) - {""}:
-                                keep_calculating = keep_calculating or add_if_not_already_added(
-                                    x, self._follow_values[f])
-                i = len(rule.rest) - 1
-                do = True
-                while i >= 0 and (do or has_epsilon[i + 1]):
-                    f = rule.rest[i]
-                    if isinstance(f, str):
-                        break
-                    for x in self._follow_values[rule.origin] - {""}:
+                    ff = self.first(
+                        rule.rest[i+1:]) if i != len(rule.rest) - 1 else {""}
+                    for x in ff - {""}:
                         keep_calculating = keep_calculating or add_if_not_already_added(
                             x, self._follow_values[f])
-                    i -= 1
+                    if "" in ff:
+                        for x in self._follow_values[rule.origin]:
+                            keep_calculating = keep_calculating or add_if_not_already_added(
+                                x, self._follow_values[f])
 
-    def first(self, symbol):
-        return self._first_values[symbol]
+    def first(self, symbols):
+        if isinstance(symbols, Symbol):
+            return self._first_values[symbols]
+        fs = set()  # First(symbols)
+        f1 = symbols[0]
+        if isinstance(f1, str):
+            fs.add(f1)
+        else:
+            for i in range(len(symbols)):
+                f = symbols[i]
+                if isinstance(f, str):
+                    break
+                fs |= self.first(f)
+                if "" not in self.first(f):
+                    break
+                fs.remove("")
+            else:
+                fs.add("")
+        return fs
 
     def follow(self, symbol):
         return self._follow_values[symbol]
 
     def _create_parse_table(self):
-        # rows = len(self.symbols)
-        # columns = len(self.terminals) + 1  # $
-        # M = [[[] for j in range(columns)] for i in range(rows)]
         M = {s: {t: [] for t in self.terminals + ["$"]} for s in self.symbols}
         for rule in self.rules:
-            fa = set()  # First(A)
-            f1 = rule.rest[0]
-            if isinstance(f1,str):
-                fa.add(f1)
-            else:
-                for i in range(len(rule.rest)):
-                    f = rule.rest[i]
-                    if isinstance(f, str):
-                        # if not f:
-                        #     fa.add(f)
-                        break
-                    fa |= self.first(f)
-                    if "" not in self.first(f):
-                        break
+            fa = self.first(rule.rest)  # First(A)
             ein = "" in fa
             if ein:
                 fa.remove("")
-            print(rule, fa, "==========")
             for t in fa:
                 M[rule.origin][t].append(rule)
             if ein:
@@ -196,22 +179,16 @@ class CFG:
     def parse_table(self):
         return self._m
 
+    def print_parse_table(self):
+        data = self.parse_table
+        headers = data[Symbol("start")].keys()
+        rows = []
+        for symbol in data.keys():
+            rows.append([symbol] + [data[symbol][header]
+                                    for header in headers])
+        with open("table.txt", "w") as file:
+            file.write(
+                str(tabulate(rows, headers=headers, tablefmt="grid")) + "\n")
+
     def __repr__(self):
         return "\n".join([str(x) for x in self.rules])
-
-
-# c = CFG(CFG_RULES_STR)
-# print(c)
-# c = CFG("""start: t e'
-# e': "+" t e' |
-# t: f t'
-# t': "*" f t' |
-# f: "(" start ")" | "id"
-# """)
-# c.calculate_firsts()
-# c.calculate_follows()
-# print(c._first_values)
-# print(c._follow_values)
-# print(c.terminals)
-# print(c.parse_table[Symbol("t")])
-# print(c.parse_table)
