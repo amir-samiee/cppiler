901f6731e98cc278c4edfa1bb87eb75fd37f730d
arshiasamimi <samimarsh44@gmail.com>
2025-01-27 10:11:28 +0330

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,13 +1,13 @@
 #include
 using namespace std;
-int main (){
-    int epsilon;
-    int s = 0, t = 10;
-    while (t >= 0){
-        cin >> s;
-        t = t - 1;
-        s = s + x;
-    }
-    cout << "sum=" << s;
-    return 0;
+int main(){
+int x;
+int s=0, t=10;
+while (t >= 0){
+cin>>x;
+t = t - 1;
+s = s + x;
+}
+cout<<"sum="<<s;
+return 0;
 }
